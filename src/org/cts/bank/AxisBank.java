27f59e859fb3444a0d05aaed06a4c4e03c30300d
ssarath76 <ssarath76@gmail.com>
2020-05-05 00:20:42 +0530

    new code

---
+++ src/org/cts/bank/AxisBank.java
@@ -0,0 +1,18 @@
+package org.cts.bank;
+
+public class AxisBank extends IcicBank
+{
+@Override
+public void deposit() {
+	System.out.println("5%");
+}
+public void fixed() {
+	System.out.println("8%");
+}
+public static void main(String[] args) {
+	AxisBank bank = new AxisBank();
+	bank.saving();
+	bank.deposit();
+	bank.fixed();
+}
+}
---
+++ src/org/cts/bank/IcicBank.java
@@ -0,0 +1,11 @@
+package org.cts.bank;
+
+public class IcicBank {
+	public void saving() {
+		System.out.println("6%");
+	}
+	public void deposit() {
+		System.out.println();
+	}
+
+}
